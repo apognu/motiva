c026d3fa2255b26a73d909572284aa9e94fbd479
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-10-07 22:22:52 +0200

    Upgrade dependencies, use fingerprints for org replacements.
    Even though rigour contains more org replacement tokens, Yente
    still uses the list in the fingerprints repository. For backward
    compatibility, we will be using it too for the foreseeable future.

--- crates/libmotiva/src/matching/replacers/company_types.rs
+++ crates/libmotiva/src/matching/replacers/company_types.rs
@@ -3,20 +3,20 @@ use std::sync::LazyLock;
 use aho_corasick::{AhoCorasick, AhoCorasickBuilder, MatchKind};
 use serde::Deserialize;
 
-use crate::matching::replacers::Dictionaries;
+use crate::matching::replacers::Fingerprints;
 
 pub(crate) static ORG_TYPES: LazyLock<(AhoCorasick, Vec<String>)> = LazyLock::new(|| {
-  let file = Dictionaries::get("names/org_types.yml").expect("could not read org types dictionary");
+  let file = Fingerprints::get("types/types.yml").expect("could not read org types dictionary");
   let dictionary = serde_yaml::from_slice::<OrgTypeDictionary>(&file.data).expect("could not unmarshal org type dictionary");
 
   let mut patterns = Vec::new();
   let mut replacements = Vec::new();
 
   for item in dictionary.types {
-    if let Some(display) = &item.display {
-      for alias in item.aliases {
+    if let Some(main) = &item.main {
+      for alias in item.forms {
         patterns.push(alias.to_lowercase());
-        replacements.push(display.to_lowercase());
+        replacements.push(main.to_lowercase());
       }
     }
   }
@@ -34,6 +34,6 @@ struct OrgTypeDictionary {
 
 #[derive(Deserialize)]
 struct OrgTypeDictionaryEntry {
-  display: Option<String>,
-  aliases: Vec<String>,
+  main: Option<String>,
+  forms: Vec<String>,
 }
