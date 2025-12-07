796358a37c9fe60bbab74c4388d19920bd69d171
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-12-07 18:25:16 +0100

    Improve testing.

--- crates/libmotiva/src/fetcher.rs
+++ crates/libmotiva/src/fetcher.rs
@@ -12,28 +12,52 @@ pub trait CatalogFetcher: Clone + Default + Send + Sync + 'static {
   fn fetch_catalog(&self, url: &str) -> impl Future<Output = anyhow::Result<Catalog>> + Send;
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub enum ManifestProtocol {
   #[default]
   Http,
   LocalFile,
 }
 
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub enum ManifestFormat {
+  #[default]
+  Json,
+  Yaml,
+}
+
 #[derive(Clone, Default)]
 pub struct HttpCatalogFetcher {
-  pub protocol: ManifestProtocol,
   pub manifest_url: Option<String>,
+  pub protocol: ManifestProtocol,
+  pub format: ManifestFormat,
 }
 
 impl HttpCatalogFetcher {
-  pub fn from_manifest_url(url: Option<String>) -> Self {
-    let protocol = match &url {
-      Some(url) if url.starts_with("https://") || url.starts_with("http://") => ManifestProtocol::Http,
-      Some(_) => ManifestProtocol::LocalFile,
-      None => ManifestProtocol::Http,
+  pub fn from_manifest_url(url: Option<String>) -> anyhow::Result<Self> {
+    let (protocol, format) = match &url {
+      Some(url) => {
+        let protocol = if url.starts_with("http://") || url.starts_with("https://") {
+          ManifestProtocol::Http
+        } else {
+          ManifestProtocol::LocalFile
+        };
+
+        let format = if url.ends_with(".json") {
+          ManifestFormat::Json
+        } else if url.ends_with(".yaml") || url.ends_with(".yml") {
+          ManifestFormat::Yaml
+        } else {
+          anyhow::bail!("unsupported file format for manifest file");
+        };
+
+        (protocol, format)
+      }
+
+      None => (ManifestProtocol::Http, ManifestFormat::Json),
     };
 
-    Self { protocol, manifest_url: url }
+    Ok(Self { protocol, format, manifest_url: url })
   }
 }
 
@@ -58,28 +82,26 @@ impl HttpCatalogFetcher {
   async fn fetch_http(&self, url: &str) -> anyhow::Result<Manifest> {
     let response = reqwest::get(url).await.context("could not reach manifest location")?;
 
-    if url.ends_with(".json") {
-      response.json().await.context("invalid manifest file")
-    } else if url.ends_with(".yml") || url.ends_with(".yaml") {
-      tracing::warn!("using a YAML manifest is deprecated, support will be removed in a future version, use JSON instead");
+    match self.format {
+      ManifestFormat::Json => response.json().await.context("invalid manifest file"),
+      ManifestFormat::Yaml => {
+        tracing::warn!("using a YAML manifest is deprecated, support will be removed in a future version, use JSON instead");
 
-      serde_yaml::from_str(&response.text().await?).context("could not parse local manifest file as YAML")
-    } else {
-      Err(anyhow::anyhow!("unknown extension for manifest file"))
+        serde_yaml::from_str(&response.text().await?).context("could not parse local manifest file as YAML")
+      }
     }
   }
 
   async fn fetch_local_file(&self, url: &str) -> anyhow::Result<Manifest> {
     let file = File::open(url)?;
 
-    if url.ends_with(".json") {
-      serde_json::from_reader(file).context("could not parse local manifest file as JSON")
-    } else if url.ends_with(".yml") || url.ends_with(".yaml") {
-      tracing::warn!("using a YAML manifest is deprecated, support will be removed in a future version, use JSON instead");
+    match self.format {
+      ManifestFormat::Json => serde_json::from_reader(file).context("could not parse local manifest file as JSON"),
+      ManifestFormat::Yaml => {
+        tracing::warn!("using a YAML manifest is deprecated, support will be removed in a future version, use JSON instead");
 
-      serde_yaml::from_reader(file).context("could not parse local manifest file as YAML")
-    } else {
-      Err(anyhow::anyhow!("unknown extension for manifest file"))
+        serde_yaml::from_reader(file).context("could not parse local manifest file as YAML")
+      }
     }
   }
 }
@@ -113,5 +135,143 @@ impl CatalogFetcher for TestFetcher {
 
   async fn fetch_catalog(&self, url: &str) -> anyhow::Result<Catalog> {
     self.catalogs.get(url).ok_or_else(|| anyhow::anyhow!("unknown catalog url")).cloned()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use wiremock::{Mock, MockServer, ResponseTemplate, matchers::*};
+
+  use crate::{
+    CatalogFetcher, HttpCatalogFetcher,
+    catalog::OPENSANCTIONS_CATALOG_URL,
+    fetcher::{ManifestFormat, ManifestProtocol},
+  };
+
+  #[test]
+  fn detect_protocol_and_format() {
+    let tests = &[
+      ("http://domain.tld/manifest.json", ManifestProtocol::Http, ManifestFormat::Json),
+      ("http://domain.tld/manifest.yaml", ManifestProtocol::Http, ManifestFormat::Yaml),
+      ("http://domain.tld/manifest.yml", ManifestProtocol::Http, ManifestFormat::Yaml),
+      ("manifest.json", ManifestProtocol::LocalFile, ManifestFormat::Json),
+      ("/path/manifest.json", ManifestProtocol::LocalFile, ManifestFormat::Json),
+    ];
+
+    for (url, protocol, format) in tests {
+      let fetcher = HttpCatalogFetcher::from_manifest_url(Some(url.to_string())).unwrap();
+
+      assert_eq!(fetcher.protocol, *protocol);
+      assert_eq!(fetcher.format, *format);
+    }
+  }
+
+  #[tokio::test]
+  async fn default_manifest() {
+    let manifest = HttpCatalogFetcher::from_manifest_url(None).unwrap().fetch_manifest().await.unwrap();
+
+    assert_eq!(manifest.catalogs[0].url, OPENSANCTIONS_CATALOG_URL);
+    assert_eq!(manifest.catalogs[0].scope.as_deref(), Some("default"));
+    assert_eq!(manifest.catalogs[0].resource_name, "entities.ftm.json");
+  }
+
+  #[test]
+  fn invalid_extension() {
+    assert!(HttpCatalogFetcher::from_manifest_url(Some("http://domain.tld/manifest.jpg".to_string())).is_err());
+  }
+
+  #[tokio::test]
+  async fn valid_local_file_json() {
+    std::fs::write(
+      "/tmp/motiva-manifest.json",
+      r#"{"catalogs":[{"url": "http://myurl.tld","scope":"myscope","resource_name":"ents.json"}]}"#,
+    )
+    .unwrap();
+
+    let manifest = HttpCatalogFetcher::from_manifest_url(Some("/tmp/motiva-manifest.json".to_string()))
+      .unwrap()
+      .fetch_manifest()
+      .await
+      .unwrap();
+
+    assert_eq!(manifest.catalogs[0].url, "http://myurl.tld");
+    assert_eq!(manifest.catalogs[0].scope.as_deref(), Some("myscope"));
+    assert_eq!(manifest.catalogs[0].resource_name, "ents.json");
+  }
+
+  #[tokio::test]
+  async fn valid_local_file_yaml() {
+    for ext in ["yml", "yaml"] {
+      std::fs::write(
+        &format!("/tmp/motiva-manifest.{ext}"),
+        r#"
+      catalogs:
+        - url: http://myurl.tld
+          scope: myscope
+          resource_name: ents.json
+      "#,
+      )
+      .unwrap();
+
+      let manifest = HttpCatalogFetcher::from_manifest_url(Some("/tmp/motiva-manifest.yml".to_string()))
+        .unwrap()
+        .fetch_manifest()
+        .await
+        .unwrap();
+
+      assert_eq!(manifest.catalogs[0].url, "http://myurl.tld");
+      assert_eq!(manifest.catalogs[0].scope.as_deref(), Some("myscope"));
+      assert_eq!(manifest.catalogs[0].resource_name, "ents.json");
+    }
+  }
+
+  #[tokio::test]
+  async fn valid_http_json() {
+    let mock = MockServer::start().await;
+
+    Mock::given(method("GET"))
+      .and(path("/manifest.json"))
+      .respond_with(ResponseTemplate::new(200).set_body_raw(r#"{"catalogs":[{"url": "http://myurl.tld","scope":"myscope","resource_name":"ents.json"}]}"#, "application/json"))
+      .mount(&mock)
+      .await;
+
+    let manifest = HttpCatalogFetcher::from_manifest_url(Some(format!("{}/manifest.json", mock.uri())))
+      .unwrap()
+      .fetch_manifest()
+      .await
+      .unwrap();
+
+    assert_eq!(manifest.catalogs[0].url, "http://myurl.tld");
+    assert_eq!(manifest.catalogs[0].scope.as_deref(), Some("myscope"));
+    assert_eq!(manifest.catalogs[0].resource_name, "ents.json");
+  }
+
+  #[tokio::test]
+  async fn valid_http_yaml() {
+    let mock = MockServer::start().await;
+
+    Mock::given(method("GET"))
+      .and(path("/manifest.yml"))
+      .respond_with(ResponseTemplate::new(200).set_body_raw(
+        r#"
+        catalogs:
+          - url: http://myurl.tld
+            scope: myscope
+            resource_name: ents.json
+        "#,
+        "application/json",
+      ))
+      .mount(&mock)
+      .await;
+
+    let manifest = HttpCatalogFetcher::from_manifest_url(Some(format!("{}/manifest.yml", mock.uri())))
+      .unwrap()
+      .fetch_manifest()
+      .await
+      .unwrap();
+
+    assert_eq!(manifest.catalogs[0].url, "http://myurl.tld");
+    assert_eq!(manifest.catalogs[0].scope.as_deref(), Some("myscope"));
+    assert_eq!(manifest.catalogs[0].resource_name, "ents.json");
   }
 }
