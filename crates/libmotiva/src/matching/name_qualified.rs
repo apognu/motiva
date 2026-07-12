8600823a8bd2776fe54d499c58b6e9f760e677be
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-07-12 15:41:20 +0200

    Align all feature computations into using run_features.

--- crates/libmotiva/src/matching/name_qualified.rs
+++ crates/libmotiva/src/matching/name_qualified.rs
@@ -5,7 +5,7 @@ use tracing::instrument;
 
 use crate::{
   matching::{
-    Feature, MatchingAlgorithm,
+    Feature, FeaturesConfig, MatchingAlgorithm,
     matchers::{
       jaro_winkler::JaroNameParts,
       mismatch::{SimpleMismatch, dob_day_disjoint, dob_year_disjoint},
@@ -38,13 +38,13 @@ impl MatchingAlgorithm for NameQualified {
   }
 
   #[instrument(name = "score_hit", skip_all, fields(algorithm = Self::name(), entity_id = rhs.id))]
-  fn score(bump: &Bump, lhs: &SearchEntity, rhs: &Entity, cutoff: f64) -> (f64, Vec<(&'static str, f64)>) {
+  fn score(bump: &Bump, lhs: &SearchEntity, rhs: &Entity, _cutoff: f64) -> (f64, Vec<(&'static str, f64)>) {
     if !rhs.schema.is_a(lhs.schema.as_str()) {
       return (0.0, vec![]);
     }
 
     let mut results = Vec::with_capacity(FEATURES.len());
-    let score = run_features(bump, lhs, rhs, 0.0, cutoff, FEATURES.iter(), &mut results);
+    let score = run_features(bump, lhs, rhs, 0.0, FeaturesConfig::summed_features(FEATURES.iter()), &mut results);
 
     (score.clamp(0.0, 1.0), results)
   }
